2f93b5e74310915cc2b7c039ccc27784e684ab44
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2024-07-14 19:08:24 +0530

    Remove the File as Content is Transferred

--- src/events.js
+++
@@ -1,7 +0,0 @@
-function addEventToBtn(customid, customEvent) {
-    const eventBtn = document.getElementById(customid);
-    eventBtn.addEventListener('click', () => {
-        eventBtn.customEvent;
-    })
-
-}
